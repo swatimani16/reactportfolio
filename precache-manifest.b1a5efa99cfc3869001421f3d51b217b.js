ecede1ff52d4d996bcd75ed89994f0d55eec028e
Swati Mani <swati.mani@mavs.uta.edu>
2020-02-11 14:12:21 -0600

    Updates

---
+++ precache-manifest.b1a5efa99cfc3869001421f3d51b217b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95555543a4fe73c17ab9970b76227ca1",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "3117536c55a282850377",
+    "url": "/reactportfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "70460e111ef6e0f472d5",
+    "url": "/reactportfolio/static/css/main.fe236fc7.chunk.css"
+  },
+  {
+    "revision": "3117536c55a282850377",
+    "url": "/reactportfolio/static/js/2.f2cb5633.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/reactportfolio/static/js/2.f2cb5633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70460e111ef6e0f472d5",
+    "url": "/reactportfolio/static/js/main.6b3124c4.chunk.js"
+  },
+  {
+    "revision": "72065c742947ebf282eb",
+    "url": "/reactportfolio/static/js/runtime-main.baef4adf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba2c35573f6fcd3db67bd7fe5c2270ca.js"
+  "/reactportfolio/precache-manifest.b1a5efa99cfc3869001421f3d51b217b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
