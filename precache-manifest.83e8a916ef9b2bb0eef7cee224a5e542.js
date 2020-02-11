a30180da236462c1cff47a87a9ccf312fe385c10
Swati Mani <swati.mani@mavs.uta.edu>
2020-02-11 13:32:09 -0600

    Updates

---
+++ precache-manifest.83e8a916ef9b2bb0eef7cee224a5e542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded82c8bb06f021d47cdc5633592dea8",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "3117536c55a282850377",
+    "url": "/reactportfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "bd587abdf113e42a2c89",
+    "url": "/reactportfolio/static/css/main.5fc82778.chunk.css"
+  },
+  {
+    "revision": "3117536c55a282850377",
+    "url": "/reactportfolio/static/js/2.f2cb5633.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/reactportfolio/static/js/2.f2cb5633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd587abdf113e42a2c89",
+    "url": "/reactportfolio/static/js/main.4bc4d240.chunk.js"
+  },
+  {
+    "revision": "72065c742947ebf282eb",
+    "url": "/reactportfolio/static/js/runtime-main.baef4adf.js"
+  }
+]);
