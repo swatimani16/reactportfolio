344d4ee03c278fe885f81127e570b4b8eaaa50cc
Swati Mani <swati.mani@mavs.uta.edu>
2020-02-23 15:15:37 -0600

    Updates

--- precache-manifest.b31453426fed4f615986ed480c4d4b34.js
+++ precache-manifest.b31453426fed4f615986ed480c4d4b34.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77664cc4d5a0cf0193202b4256c8253b",
+    "revision": "137d67ddd67c8db52d77cc4899423530",
     "url": "/reactportfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactportfolio/static/css/2.a6bc1c31.chunk.css"
   },
   {
-    "revision": "499af8a6fbc9f159b4b3",
-    "url": "/reactportfolio/static/css/main.0274f1f8.chunk.css"
+    "revision": "6afae463bc46c99dbbfd",
+    "url": "/reactportfolio/static/css/main.ba74b6b8.chunk.css"
   },
   {
     "revision": "3117536c55a282850377",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactportfolio/static/js/2.f2cb5633.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "499af8a6fbc9f159b4b3",
+    "revision": "6afae463bc46c99dbbfd",
     "url": "/reactportfolio/static/js/main.bcbe030d.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactportfolio/precache-manifest.68325c9d85e43a09076feb4882362a17.js"
+  "/reactportfolio/precache-manifest.b31453426fed4f615986ed480c4d4b34.js"
 );
 
 self.addEventListener('message', (event) => {
