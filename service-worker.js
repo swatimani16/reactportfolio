907b5cb88b801820b1fe6a81308469cfbeb4fc19
Swati Mani <swati.mani@mavs.uta.edu>
2020-08-06 23:22:21 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactportfolio/precache-manifest.38bea0860ebec67a1125164e28a13cfb.js"
+  "/reactportfolio/precache-manifest.d8266b59b811eefaa067c09f4c2e449e.js"
 );
 
 self.addEventListener('message', (event) => {
